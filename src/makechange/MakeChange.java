41d3f158c1b422d8bc90d2fe593d38176d7bd07b
Joseph Herrera <tony.herrera90@gmail.com>
2020-08-29 10:00:20 -0500

    skeleton outline

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,7 +1,44 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 	public static void main(String[] args) {
+		//import scanner		
+		Scanner sc = new Scanner(System.in);
+		
+		//Declare variables
+		
+		
+		// Create area to call methods
+		StoryOne();
+		StoryTwo();
+		StoryThree();
+		StoryOne();
+
+		
+	}
+
+	private static void StoryOne() {
+		Scanner sc = new Scanner(System.in);
+		//prompt user to state the price of the item
+		
+	}
+
+	private static void StoryTwo() {
+		Scanner sc = new Scanner(System.in);
+		//prompt user to state how much money was offered to pay for item
+		
+	}
+
+	private static void StoryThree() {
+		Scanner sc = new Scanner(System.in);
+		//Create a if else statement displaying whether or not they offered the right amount or too little
+	}
+
+	private static void StoryFour() {
+		Scanner sc = new Scanner(System.in);
+		// create an else statement giving the user their correct amount of change
 		
 	}
 
