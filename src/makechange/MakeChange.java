1c7057c8be7c97cdb0e28be1ae683818b5104a16
Joseph Herrera <tony.herrera90@gmail.com>
2020-08-29 12:01:31 -0500

    updated code

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -4,42 +4,57 @@ import java.util.Scanner;
 
 public class MakeChange {
 	public static void main(String[] args) {
-		//import scanner		
+		// Import scanner
 		Scanner sc = new Scanner(System.in);
-		
-		//Declare variables
-		
-		
-		// Create area to call methods
-		StoryOne();
-		StoryTwo();
-		StoryThree();
-		StoryOne();
 
+		// Declare variables
+		double prodPrice, amtTendered, cashBack, maxPrice;
+//		int pennies, nickels, dimes, quarters, dollars, fives, tens, twenties;
+
+		// Initialize variables
+//		double Pennies = .01;
+//		double Nickels = .05;
+//		double Dimes = .10;
+//		double Quarters = .25;
+//		double Dollars = 1.00;
+//		double Fives = 5.00;
+//		double Tens = 10.00;
+//		double Twenties = 20.00;
+//		boolean keepGoing = true;
+
+		maxPrice = 20;
+
+		// Prompt user to input the item's price
+		System.out.println("Please, enter the price for the item: ");
+		prodPrice = sc.nextDouble();
+		System.out.println(prodPrice);
+		System.out.println("Please, enter amount tendered: ");
+		amtTendered = sc.nextDouble();
+		if (amtTendered < prodPrice) {
+			System.out.println("Please enter the correct amount of money");
+		} else if (amtTendered == prodPrice) {
+			System.out.println("Have a nice day. Come again");
+		} else {
+			System.out.println(amtTendered - prodPrice);
+		}
 		
+
+//		while (prodPrice <= maxPrice) 	
+//		{System.out.println("Please, enter the amount tendered: ");
+//			amtTendered = sc.nextDouble();
+
+//			if (prodPrice > maxPrice) 
+//			{System.out.println("We can't afford those items. Spend less money");
+//			return;
+//																	} 
+//
+//				 if (amtTendered > maxPrice) {
+//					System.out.println("Please, offer amount less than $20.");
+//					return;
+//				}
+//			} 
+		}
 	}
 
-	private static void StoryOne() {
-		Scanner sc = new Scanner(System.in);
-		//prompt user to state the price of the item
-		
-	}
 
-	private static void StoryTwo() {
-		Scanner sc = new Scanner(System.in);
-		//prompt user to state how much money was offered to pay for item
-		
-	}
 
-	private static void StoryThree() {
-		Scanner sc = new Scanner(System.in);
-		//Create a if else statement displaying whether or not they offered the right amount or too little
-	}
-
-	private static void StoryFour() {
-		Scanner sc = new Scanner(System.in);
-		// create an else statement giving the user their correct amount of change
-		
-	}
-
-}
