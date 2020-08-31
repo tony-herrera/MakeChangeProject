c6bd02ef732e6d37367dea658037204f335097ee
Joseph Herrera <tony.herrera90@gmail.com>
2020-08-31 09:11:45 -0500

    Master Copy

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -9,6 +9,7 @@ public class MakeChange {
 		// Declare variables
 		double prodPrice, amtTendered, cashBack, maxPrice;
 		maxPrice = 20.0;
+		int changeAmt = (int) .00000005;
 		// Prompt user to input the item's price
 		System.out.println("Please, enter the price for the item: ");
 		prodPrice = sc.nextDouble();
@@ -22,21 +23,23 @@ public class MakeChange {
 		} else {
 			System.out.println(amtTendered - prodPrice);
 			// Declare cashBack variable and initialize
-			cashBack = amtTendered - prodPrice;
+			cashBack = (.00000005 + amtTendered) - prodPrice;
 			int tens = (int) (cashBack / 10);
 			cashBack = cashBack - (tens * 10);
 			int fives = (int) (cashBack / 5);
 			cashBack = cashBack - (fives * 5);
 			int dollars = (int) (cashBack / 1);
 			cashBack = cashBack - (dollars * 1);
+			
 			int quarters = (int) (cashBack / .25);
 			cashBack = cashBack - (quarters * .25);
 			int dimes = (int) (cashBack / .10);
 			cashBack = cashBack - (dimes * .10);
 			int nickels = (int) (cashBack / .05);
 			cashBack = cashBack - (nickels * .05);
-			int pennies = (int) (cashBack /.01);
+			int pennies = (int) (cashBack /.01 );
 			cashBack = cashBack - (pennies * .01);
+			if (pennies != 0);
 			System.out.println("Here is your change: " + tens + " tens " + fives + " fives " + dollars + " dollars "
 					+ quarters + " Quarters, " + dimes + " Dimes," + nickels + " Nickels, " + pennies + " Pennies "
 					+ " Have a nice day!");
